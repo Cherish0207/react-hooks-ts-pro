a27278d3ccacc44ba432a945375aaba5082aef59
Cherish0207 <cheshire_1207@foxmail.com>
2021-12-15 22:17:41 +0800

    6.HomeHeader:react-transition-group动画效果

--- src/routes/Home/components/HomeHeader/index.tsx
+++ src/routes/Home/components/HomeHeader/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, CSSProperties } from "react";
+import { Transition } from "react-transition-group";//react-motion好用
 import classnames from "classnames";
 import { Icon } from "antd";
 import logo from "@/assets/images/logo.png";
@@ -7,6 +8,25 @@ import "./index.less";
 //let logo = require('../../../../assets/images/logo.png');
 //如果是用require加载的话，返回值的default属性才是那个图片地址
 //如果你非要用import如何解决?
+const duration = 1000; //动画的持续时间
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+};
+//CSSProperties其实就是行内样式的对象定义
+interface TransitionStyles {
+  entering: CSSProperties;
+  entered: CSSProperties;
+  exiting: CSSProperties;
+  exited: CSSProperties;
+}
+//在不同阶段给不同样式
+const transitionStyles: TransitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+};
 
 interface Props {
   currentCategory: string; //当前选中的分类 此数据会放在redux仓库中
@@ -26,35 +46,50 @@ function HomeHeader(props: Props) {
         <img src={logo} />
         <Icon type="bars" onClick={() => setIsMenuVisible(!isMenuVisible)} />
       </div>
-      {isMenuVisible && (
-        <ul className="category" onClick={setCurrentCategory}>
-          <li
-            data-category="all"
-            className={classnames({
-              active: props.currentCategory === "all",
-            })}
+      <Transition in={isMenuVisible} timeout={duration}>
+        {(state: keyof TransitionStyles) => (
+          <ul
+            className="category"
+            onClick={setCurrentCategory}
+            style={{
+              ...defaultStyle,
+              ...transitionStyles[state],
+            }}
           >
-            全部课程
-          </li>
-          <li
-            data-category="react"
-            className={classnames({
-              active: props.currentCategory === "react",
-            })}
-          >
-            React课程
-          </li>
-          <li
-            data-category="vue"
-            className={classnames({
-              active: props.currentCategory === "vue",
-            })}
-          >
-            Vue课程
-          </li>
-        </ul>
-      )}
+            <li
+              data-category="all"
+              className={classnames({
+                active: props.currentCategory === "all",
+              })}
+            >
+              全部课程
+            </li>
+            <li
+              data-category="react"
+              className={classnames({
+                active: props.currentCategory === "react",
+              })}
+            >
+              React课程
+            </li>
+            <li
+              data-category="vue"
+              className={classnames({
+                active: props.currentCategory === "vue",
+              })}
+            >
+              Vue课程
+            </li>
+          </ul>
+        )}
+      </Transition>
     </header>
   );
 }
 export default HomeHeader;
+/**
+ * 动画是如何实现的？
+ *  Transition
+ * 动态给一个元素增加/删除类名,不同类名对应不同样式
+ * 另外再加一个tansition效果就可以了
+ */
